b0e80527cc4dae2932eebbd66ee78233fd910744
noname01 <hjc.noname@gmail.com>
2014-12-22 21:18:57 -0800

    socket.io PvP logic

--- server.js
+++ server.js
@@ -16,11 +16,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', routes.index);
 app.get("*", routes.index);
 
+var waiting = null;
+var roomCount = 0;
+
 // socket.io
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+io.on("connection", function(socket){
+    // namespace: roomX
+    var room = "room";
+    if(waiting === null){ // create a new room
+        roomCount++;
+        room += roomCount;
+        waiting = room;
+        //console.log("create " + room);
+        socket.join(room);
+        socket.emit("waiting");
+    }
+    else{ // join a room
+        room += roomCount;
+        //console.log("join room" + roomCount);
+        socket.join(room);
+        io.in(room).emit("find opponent");
+        waiting = null;
+    }
+
+    //console.log('a user connected');
+    socket.on("message", function(msg){
+        console.log("msg: " + msg);
+        io.in(room).emit("message", msg);
+    });
+    socket.on("disconnect", function(){
+        console.log("user disconnected");
+        if(waiting === room){
+            waiting = null;
+        }
+        io.in(room).emit("opponent left");
     });
 });
 
